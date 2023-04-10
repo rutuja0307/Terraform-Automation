878014cff3ff12154d2ce83b9acbaaaf7509a7fe
rutuja0307 <rutujachandurkar03@gmail.com>
2023-04-10 21:25:22 +0530

    this is first commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-6"
+    bucket = "rutuja-bucket-10-04"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
