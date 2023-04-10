a54a6b5d052d2476c0e56e4626126f4651c32bc1
rutuja0307 <rutujachandurkar03@gmail.com>
2023-04-10 23:29:54 +0530

    add ami

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-06e46074ae430fba6" 
 }
 variable "versioning" {
     type        = bool
