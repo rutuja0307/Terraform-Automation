f98f12c3a5ded68f68f0c2fc33013055e284d64d
rutuja0307 <rutujachandurkar03@gmail.com>
2023-04-10 22:39:14 +0530

    change bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rutuja-bucket-10-04"
+    bucket = "my-aws-tf-state-bucket-09876"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
